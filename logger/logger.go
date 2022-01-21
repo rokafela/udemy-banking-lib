158b1b4781f4a066c826fef9d030b23c1f9ba8f7
Arief Darmawan <ariefdwn.nutech@gmail.com>
2022-01-21 14:25:46 +0700

    upload errs and logger lib

---
+++ logger/logger.go
@@ -0,0 +1,45 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var log *zap.Logger
+
+func init() {
+	var err error
+
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.TimeKey = "timestamp"
+	encoderConfig.MessageKey = "message"
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	config := zap.NewProductionConfig()
+	config.EncoderConfig = encoderConfig
+
+	log, err = config.Build(zap.AddCallerSkip(1))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Info(message string, fields ...zap.Field) {
+	log.Info(message, fields...)
+}
+
+func Debug(message string, fields ...zap.Field) {
+	log.Debug(message, fields...)
+}
+
+func Error(message string, fields ...zap.Field) {
+	log.Error(message, fields...)
+}
+
+func Fatal(message string, fields ...zap.Field) {
+	log.Fatal(message, fields...)
+}
+
+func Panic(message string, fields ...zap.Field) {
+	log.Panic(message, fields...)
+}
